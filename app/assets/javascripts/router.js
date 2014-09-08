278be65837a8a033fd8bb9edc7fb2f3eefe6aa83
squit <michael.squitieri@gmail.com>
2014-09-07 21:58:00 -0400

    router now uses location api

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,5 +1,9 @@
 // For more information see: http://emberjs.com/guides/routing/
 
+Reddit.Router.reopen({
+  location: 'auto'
+});
+
 Reddit.Router.map(function() {
   this.resource('links', { path: '/' }, function() {
     this.route('new', {path: '/links/new'});
